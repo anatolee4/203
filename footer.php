bb783035b85faa46d7aeed0a7e9ff085eb93d25b
anatolee4 <anatole.martin@sfr.fr>
2026-06-02 10:49:27 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer class="footer-section">
+        <div class="footer-main">
+            <h2 class="footer-logo">E-llusion</h2>
+            
+            <div class="footer-content">
+                <div class="footer-brand-left">
+                    <img src="chemin_vers_mmi_logo.png" alt="MMI Chambéry" class="partner-logo">
+                </div>
+
+                <div class="footer-center">
+                    <p class="contact-mail">nous contacter : <a href="mailto:aaaa@univ-smb.fr">aaaa@univ-smb.fr</a></p>
+                    <a href="https://instagram.com" target="_blank" class="instagram-link">
+                        <img src="chemin_vers_insta_icon.png" alt="" class="insta-icon"> Instagram
+                    </a>
+                </div>
+
+                <div class="footer-brand-right">
+                    <img src="chemin_vers_iut_logo.png" alt="IUT Chambéry" class="partner-logo">
+                </div>
+            </div>
+        </div>
+        
+        <div class="footer-sub">
+            <a href="https://mmi.univ-smb.fr" target="_blank">https://mmi.univ-smb.fr/</a>
+        </div>
+    </footer>
