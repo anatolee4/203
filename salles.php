e233334d0f6822483c94b2b91c79acecfa1a12cc
anatolee4 <anatole.martin@sfr.fr>
2026-06-03 09:29:03 +0200

    header dans salle

--- salles.php
+++ salles.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-llusion - Les salles</title>
     <link rel="stylesheet" href="salles.css">
+    <link rel="stylesheet" href="footer.css">
+    
 </head>
 <body>
     <header class="header-container-absolute">
