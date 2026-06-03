8f0654e85c60854e9af394bbbf6c48e6b1acdb49
Luc <luc.palisse09@gmail.com>
2026-06-03 09:00:35 +0200

    inscription

---
+++ inscription4.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-llusion - Exposition Interactive</title>
+    <link rel="stylesheet" href="menu.css">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="footer.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap" rel="stylesheet">
+</head>
+    <header class="header-container-absolute">
+        <?php include 'header.php'; ?>
+    </header>
