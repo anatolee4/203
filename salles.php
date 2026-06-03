4170bffc1a32653d6fea6d0ce84980c8534020d2
anatolee4 <anatole.martin@sfr.fr>
2026-06-03 09:00:37 +0200

    sall2

--- salles.php
+++ salles.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-llusion - Les salles</title>
-    <link rel="stylesheet" href="salle.css">
+    <link rel="stylesheet" href="salles.css">
 </head>
 <body>
     <main class="salles-page">
