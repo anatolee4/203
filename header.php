129925a4970729986e6cbac48a0d52cb72197df8
Luc <luc.palisse09@gmail.com>
2026-06-03 10:32:10 +0200

    lien

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <nav class="site-header" aria-label="Navigation principale">
     <link rel="stylesheet" href="header.css">
     <ul class="site-header__nav">
-        <li><a href="/203/203/inscription1.php" class="site-header__link">Inscription</a></li>
-        <li><a href="/203/203/salles.php" class="site-header__link">Les salles</a></li>
-        <li><a href="/203/203/oeuvres.php" class="site-header__link">Les oeuvres</a></li>
+        <li><a href="inscription1.php" class="site-header__link">Inscription</a></li>
+        <li><a href="salles.php" class="site-header__link">Les salles</a></li>
+        <li><a href="oeuvres.php" class="site-header__link">Les oeuvres</a></li>
     </ul>
 
-    <a href="/203/203/menu.php" class="site-header__logo" aria-label="Accueil E-llusion">
+    <a href="menu.php" class="site-header__logo" aria-label="Accueil E-llusion">
         <span class="site-header__logo-e">E</span>-llusion
     </a>
 </nav>
